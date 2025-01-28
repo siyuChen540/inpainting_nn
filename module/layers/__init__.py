d88c4ed4ccae4275c4ebc4a510b732877c70f75d
siyuChen540 <86223697+siyuChen540@users.noreply.github.com>
2025-01-28 11:04:08 +0800

    Update __init__.py

--- module/layers/__init__.py
+++ module/layers/__init__.py
@@ -1,11 +1,20 @@
 from .basic import DSConv2d, SELayer
-from .recurrent import ConvLSTM_cell, ConvGRU_cell, ConvGRU_cell_v2, FTCGRUCell
+from .recurrent import BaseFrequencyRNNCell, ConvLSTMCell, ConvGRUCell, ConvGRUCellV2, FTCGRUCell
 
 __all__ = [
     'DSConv2d',
     'SELayer',
-    'ConvLSTM_cell',
-    'ConvGRU_cell',
-    'ConvGRU_cell_v2',
+    'BaseFrequencyRNNCell',
+    'ConvLSTMCell',
+    'ConvGRUCell',
+    'ConvGRUCellV2',
     'FTCGRUCell'
-] 
+]
+
+__version__ = '0.2.1'
+
+__annotations__ = {
+    'version': str
+}
+
+__author__ = 'Siyu Chen'
